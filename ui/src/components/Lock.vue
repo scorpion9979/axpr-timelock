7c49a96a030012c53f83cfc3d1e8d323872b6a20
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-08-01 18:17:42 +0300

    Updated UI

--- ui/src/components/Lock.vue
+++ ui/src/components/Lock.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="lock section has-text-centered">
-    <div class="container">
-      <div class="field">
-        <div class="control has-icons-left">
-          <input class="input is-large" placeholder="Recipient">
-          <span class="icon is-small is-left">
-            <i class="fab fa-ethereum"></i>
-          </span>
+  <div class="lock has-text-centered">
+    <div class="columns is-centered is-mobile">
+      <div class="column is-three-fifths-desktop is-four-fifths-mobile">
+        <div class="container">
+          <div class="field">
+            <div class="control has-icons-left">
+              <input class="input is-large" placeholder="Recipient">
+              <span class="icon is-small is-left">
+                <i class="fab fa-ethereum"></i>
+              </span>
+            </div>
+          </div>
+          <div class="field">
+            <div class="control has-icons-left">
+              <input class="input is-medium" type="number" placeholder="Amount">
+              <span class="icon is-small is-left">
+                <i class="fas fa-coins"></i>
+              </span>
+            </div>
+          </div>
+          <b-field>
+              <b-datepicker
+                  placeholder="Date for release"
+                  icon="calendar-today"
+                  size="is-medium"
+                  editable>
+              </b-datepicker>
+          </b-field>
+          <!-- <div class="field">
+            <div class="control has-icons-left">
+              <input class="input is-large" type="date" placeholder="Date for release"/>
+              <span class="icon is-small is-left">
+                <i class="fas fa-calendar-alt"></i>
+              </span>
+            </div>
+          </div> -->
+          <a class="button is-warning is-large">Lock</a>
         </div>
       </div>
-      <div class="field">
-        <div class="control has-icons-left">
-          <input class="input is-medium" type="number" placeholder="Amount">
-          <span class="icon is-small is-left">
-            <i class="fas fa-coins"></i>
-          </span>
-        </div>
-      </div>
-      <b-field>
-          <b-datepicker
-              placeholder="Date for release"
-              icon="calendar-today"
-              size="is-medium"
-              editable>
-          </b-datepicker>
-      </b-field>
-      <!-- <div class="field">
-        <div class="control has-icons-left">
-          <input class="input is-large" type="date" placeholder="Date for release"/>
-          <span class="icon is-small is-left">
-            <i class="fas fa-calendar-alt"></i>
-          </span>
-        </div>
-      </div> -->
-      <a class="button is-warning is-large">Lock</a>
     </div>
   </div>
 </template>
--- ui/src/components/Release.vue
+++ ui/src/components/Release.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="release section has-text-centered">
-    <div class="container">
-      <div class="field">
-        <div class="control has-icons-left">
-          <input class="input is-large" placeholder="Recipient">
-          <span class="icon is-small is-left">
-            <i class="fab fa-ethereum"></i>
-          </span>
+  <div class="release has-text-centered">
+    <div class="columns is-centered is-mobile">
+      <div class="column is-three-fifths-desktop is-four-fifths-mobile">
+        <div class="container">
+          <div class="field">
+            <div class="control has-icons-left">
+              <input class="input is-large" placeholder="Recipient">
+              <span class="icon is-small is-left">
+                <i class="fab fa-ethereum"></i>
+              </span>
+            </div>
+          </div>
+          <a class="button is-warning is-large">Release</a>
         </div>
       </div>
-      <a class="button is-warning is-large">Release</a>
     </div>
   </div>
 </template>
