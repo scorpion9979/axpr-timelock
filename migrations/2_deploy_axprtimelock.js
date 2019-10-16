7cef429802309e47e636448d98eb4bd2ac71bf47
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-10-16 05:21:05 +0300

    Re-migrated to Ropsten

--- migrations/2_deploy_axprtimelock.js
+++ migrations/2_deploy_axprtimelock.js
@@ -5,10 +5,12 @@ const FakeAxpr = artifacts.require("FakeAxpr");
 module.exports = function(deployer, network, accounts) {
   if (network == "live") {
     // Live AXPR token
-    deployer.deploy(Timelock, "0xC39E626A04C5971D770e319760D7926502975e47");
+    deployer.deploy(Timelock, process.env.ROPSTEN_AXPR_CONTRACT);
   } else if (network == "ropsten") {
     // Ropsten FakeAxpr token
-    deployer.deploy(Timelock, process.env.ROPSTEN_AXPR_CONTRACT);
+    deployer.deploy(FakeAxpr).then(function() {
+      deployer.deploy(Timelock, FakeAxpr.address);
+    });
   } else if (network == "development") {
     const [timelockOwner, axprOwner] = accounts;
     deployer.deploy(FakeAxpr, { from: axprOwner }).then(function() {
